969d3fbed684700e2bc4d94328f8567ac88be60e
Krishna Tiwari <tshakshi54@gmail.com>
2023-05-07 16:01:09 +0530

    Add image to profile

--- src/student/RegisterStudent.java
+++ src/student/RegisterStudent.java
@@ -1,9 +1,12 @@
 package student;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
@@ -18,6 +21,8 @@ public class RegisterStudent extends JFrame implements ActionListener {
 	JButton eVerify, mVerify, altVerify, next, back;
 	JLabel PersDet, NameLabel, FNameLabel, MNameLabel, GenderLabel, DOBLabel, EMailLabel, MobLabel, AltMobLabel, UniqID,
 			PassLabel, CPassLabel, ID;
+	private JLabel fileLabel;
+	private JButton fileButton;
 
 	public RegisterStudent() {
 
@@ -26,6 +31,7 @@ public class RegisterStudent extends JFrame implements ActionListener {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JPanel p = new JPanel();
+		p.setOpaque(false);
 		p.setLayout(null);
 		p.setBackground(new Color(173, 216, 230));
 		add(p);
@@ -45,6 +51,44 @@ public class RegisterStudent extends JFrame implements ActionListener {
 		Name = new JTextField();
 		Name.setBounds(150, 50, 200, 20);
 		p.add(Name);
+
+		fileLabel = new JLabel("Add Photo:");
+		fileLabel.setBounds(400, 50, 150, 20);
+		p.add(fileLabel);
+
+		fileButton = new JButton("Choose File");
+		fileButton.setBounds(470, 50, 150, 20);
+		fileButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("Image Files", "PNG", "jpeg");
+				fileChooser.setFileFilter(filter);
+				int result = fileChooser.showOpenDialog(fileButton);
+				if (result == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = fileChooser.getSelectedFile();
+					fileButton.setText("Change File");
+
+					ImageIcon i1 = new ImageIcon(selectedFile.getAbsolutePath());
+					Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+					ImageIcon i3 = new ImageIcon(i2);
+					JLabel image = new JLabel(i3);
+					
+					Component[] components = p.getComponents();
+			        for (Component c : components) {
+			            if (c instanceof JLabel && ((JLabel) c).getIcon() != null) {
+			                p.remove(c);
+			            }
+			        }
+					
+					image.setBounds(470, 80, 100, 100);
+					p.add(image, BorderLayout.CENTER);
+					p.revalidate(); 
+					p.repaint(); 
+				}
+			}
+		});
+		p.add(fileButton);
 
 		FNameLabel = new JLabel("Father's Name : ");
 //		FNameLabel.setBackground(new Color(0, 0, 230));
@@ -112,34 +156,34 @@ public class RegisterStudent extends JFrame implements ActionListener {
 				}
 			}
 		});
-		
+
 		EMailLabel = new JLabel("Email : ");
 		EMailLabel.setBounds(20, 200, 120, 20);
 		EMailLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		p.add(EMailLabel);
-		
+
 		email = new JTextField();
 		email.setBounds(150, 200, 200, 20);
 		p.add(email);
-		
+
 		MobLabel = new JLabel("Mobile No. : ");
 		MobLabel.setBounds(20, 230, 120, 20);
 		MobLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		p.add(MobLabel);
-		
+
 		MobNum = new JTextField();
 		MobNum.setBounds(150, 230, 200, 20);
 		p.add(MobNum);
-		
+
 		AltMobLabel = new JLabel("Alternate No. : ");
 		AltMobLabel.setBounds(20, 260, 120, 20);
 		AltMobLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		p.add(AltMobLabel);
-		
+
 		AltMobNum = new JTextField();
 		AltMobNum.setBounds(150, 260, 200, 20);
 		p.add(AltMobNum);
-		
+
 		UniqID = new JLabel("Unique ID : ");
 		UniqID.setBounds(20, 290, 120, 20);
 		UniqID.setFont(new Font("Tahoma", Font.PLAIN, 15));
@@ -151,48 +195,47 @@ public class RegisterStudent extends JFrame implements ActionListener {
 		ID.setOpaque(true);
 		ID.setBackground(Color.lightGray);
 		p.add(ID);
-		
+
 		PassLabel = new JLabel("Password* :");
 		PassLabel.setBounds(20, 320, 120, 20);
-        p.add(PassLabel);
-        
-        Password = new JPasswordField();
-        Password.setBounds(150, 320, 200, 20);
-        p.add(Password);
-        
-        
-        CPassLabel = new JLabel("Confirm Password* :");
-        CPassLabel.setBounds(20, 350, 120, 20);
-        p.add(CPassLabel);
-        
-        CPassword = new JPasswordField();
-        CPassword.setBounds(150, 350, 200, 20);
-        p.add(CPassword);
-        
-        back = new JButton("Back to LogIn");
-        back.setForeground(Color.BLACK);
-        back.setBounds(150, 400, 120, 25);
-        back.addActionListener(this);
-        p.add(back);
-        
-        next = new JButton("Save and Next");
-        next.setForeground(Color.BLACK);
-        next.setBounds(300, 400, 120, 25);
-        next.addActionListener(this);
-        p.add(next);
-		
+		p.add(PassLabel);
+
+		Password = new JPasswordField();
+		Password.setBounds(150, 320, 200, 20);
+		p.add(Password);
+
+		CPassLabel = new JLabel("Confirm Password* :");
+		CPassLabel.setBounds(20, 350, 120, 20);
+		p.add(CPassLabel);
+
+		CPassword = new JPasswordField();
+		CPassword.setBounds(150, 350, 200, 20);
+		p.add(CPassword);
+
+		back = new JButton("Back to LogIn");
+		back.setForeground(Color.BLACK);
+		back.setBounds(150, 400, 120, 25);
+		back.addActionListener(this);
+		p.add(back);
+
+		next = new JButton("Save and Next");
+		next.setForeground(Color.BLACK);
+		next.setBounds(300, 400, 120, 25);
+		next.addActionListener(this);
+		p.add(next);
+
 		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource() == next) {
+		if (e.getSource() == next) {
 			setVisible(false);
-            new RegisterStudent2();
+			new RegisterStudent2();
 		}
-		
-		if(e.getSource() == back) {
+
+		if (e.getSource() == back) {
 			setVisible(false);
 			new Login();
 		}
--- src/student/RegisterStudent4.java
+++ src/student/RegisterStudent4.java
@@ -1,35 +1,192 @@
 package student;
 
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
-public class RegisterStudent4 extends JFrame implements ActionListener{
-	
+public class RegisterStudent4 extends JFrame implements ActionListener {
+
 	private JPanel p;
+	private JLabel headingLabel;
+	private JLabel boardLabel;
+	private JComboBox<String> boardList;
+	private JLabel schoolNameLabel;
+	private JTextField schoolNameField;
+	private Component enrollmentNumberLabel;
+	private JTextField enrollmentNumberField;
+	private JLabel percentageLabel;
+	private JTextField percentageField;
+	private Component yearOfPassingLabel;
+	private Component yearOfPassingField;
+	private Component fileLabel;
+	private JButton fileButton;
+	private JComponent back2;
+	private JButton addButton;
 	
+
 	public RegisterStudent4() {
 		super("Praptank | Student Registration");
 		setBounds(250, 50, 900, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		
 		p = new JPanel();
 		p.setLayout(null);
 		p.setBackground(new Color(173, 216, 230));
 		add(p);
 		
+		headingLabel = new JLabel("Certifications");
+		headingLabel.setBounds(150, 20, 200, 30);
+		p.add(headingLabel);
+
+		boardLabel = new JLabel("Board:");
+		boardLabel.setBounds(30, 60, 150, 20);
+		p.add(boardLabel);
+
+		String[] boards = { "Select", "CBSE", "State Board", "ICSE" };
+
+		boardList = new JComboBox<String>(boards);
+		boardList.setBounds(180, 60, 200, 20);
+		p.add(boardList);
+
+		schoolNameLabel = new JLabel("School Name:");
+		schoolNameLabel.setBounds(30, 90, 150, 20);
+		p.add(schoolNameLabel);
+
+		schoolNameField = new JTextField();
+		schoolNameField.setBounds(180, 90, 200, 20);
+		p.add(schoolNameField);
+
+		enrollmentNumberLabel = new JLabel("Enrollment Number:");
+		enrollmentNumberLabel.setBounds(30, 120, 150, 20);
+		p.add(enrollmentNumberLabel);
+
+		enrollmentNumberField = new JTextField();
+		enrollmentNumberField.setBounds(180, 120, 200, 20);
+		p.add(enrollmentNumberField);
+
+		percentageLabel = new JLabel("Percentage:");
+		percentageLabel.setBounds(30, 150, 150, 20);
+		p.add(percentageLabel);
+
+		percentageField = new JTextField();
+		percentageField.setBounds(180, 150, 200, 20);
+		p.add(percentageField);
+
+		yearOfPassingLabel = new JLabel("Year of Passing:");
+		yearOfPassingLabel.setBounds(30, 180, 150, 20);
+		p.add(yearOfPassingLabel);
+
+		yearOfPassingField = new JTextField();
+		yearOfPassingField.setBounds(180, 180, 200, 20);
+		p.add(yearOfPassingField);
+
+		fileLabel = new JLabel("Marksheet:");
+		fileLabel.setBounds(30, 210, 150, 20);
+		p.add(fileLabel);
+
+		fileButton = new JButton("Choose File");
+		fileButton.setBounds(180, 210, 150, 20);
+		fileButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & PDF Images", "jpg", "pdf");
+				fileChooser.setFileFilter(filter);
+				int result = fileChooser.showOpenDialog(fileButton);
+				if (result == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = fileChooser.getSelectedFile();
+					String fileName = selectedFile.getName();
+					fileLabel.setText("Marksheet:                           " + fileName);
+					fileLabel.setBounds(30, 210, 350, 20);
+					fileButton.setText("Change File");
+					fileButton.setBounds(400, 210, 150, 20);
+				}
+			}
+		});
+		p.add(fileButton);
+
+		back2 = new JButton("←");
+		back2.setForeground(Color.black);
+		back2.setBounds(0, 0, 50, 20);
+		back2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				new RegisterStudent3();
+
+			}
+		});
+		p.add(back2);
+
+		addButton = new JButton("Add");
+		addButton.setBounds(150, 260, 100, 20);
+		addButton.addActionListener(new ActionListener() {
+			private Component successLabel;
+			private AbstractButton addMoreButton;
+
+			public void actionPerformed(ActionEvent e) {
+				// Clear the input fields
+				boardList.setSelectedIndex(0);
+				schoolNameField.setText("");
+				enrollmentNumberField.setText("");
+				percentageField.setText("");
+				yearOfPassingField.setText("");
+				fileLabel.setText("Marksheet:");
+
+				p.removeAll();
+				p.revalidate();
+				p.repaint();
+				setBounds(100, 100, 400, 400);
+
+				successLabel = new JLabel(educationLevel + " details added successfully!");
+				successLabel.setBounds(100, 80, 300, 30);
+				p.add(successLabel);
+
+				addMoreButton = new JButton("Add More Education");
+				addMoreButton.setBounds(100, 120, 150, 20);
+				addMoreButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+						new RegisterStudent3();
+					}
+				});
+				p.add(addMoreButton);
+
+				completeButton = new JButton("Save and next");
+				completeButton.setBounds(100, 150, 150, 20);
+				completeButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+						new RegisterStudent4();
+					}
+				});
+				p.add(completeButton);
+			}
+		});
+		p.add(addButton);
 		
-		
+
 		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
